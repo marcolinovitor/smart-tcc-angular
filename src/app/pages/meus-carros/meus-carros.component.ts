376ef07fa39990f56f822f4ebdad55e3c3904264
Vitor Marcolino <marcolino.vitor@gmail.com>
2020-06-09 21:25:12 -0300

    Adicionando tela para cadastrar veiculo

--- src/app/pages/meus-carros/meus-carros.component.ts
+++ src/app/pages/meus-carros/meus-carros.component.ts
@@ -1,15 +1,79 @@
 import { Component, OnInit } from '@angular/core';
+import { trigger, state, style, transition, animate, keyframes } from '@angular/animations';
+import { MeusCarrosService } from './meus-carros.service';
+import { MeuVeiculo } from './model/meu-veiculo';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
-  selector: 'app-meus-carros',
-  templateUrl: './meus-carros.component.html',
-  styleUrls: ['./meus-carros.component.scss']
+    selector: 'app-meus-carros',
+    templateUrl: './meus-carros.component.html',
+    styleUrls: ['./meus-carros.component.scss'],
+    animations: [
+        trigger('row', [
+            state('ready', style({ opacity: 1 })),
+            transition('void => ready', animate('300ms 0s ease-in', keyframes([
+                style({ opacity: 0, transform: 'translateX(-30px)', offset: 0 }),
+                style({ opacity: 0.8, transform: 'translateX(10px)', offset: 0.8 }),
+                style({ opacity: 1, transform: 'translateX(0px)', offset: 1 })
+            ]))),
+            transition('ready => void', animate('300ms 0s ease-out', keyframes([
+                style({ opacity: 1, transform: 'translateX(0px)', offset: 0 }),
+                style({ opacity: 0.8, transform: 'translateX(-10px)', offset: 0.2 }),
+                style({ opacity: 0, transform: 'translateX(30px)', offset: 1 })
+            ]))),
+        ])
+    ]
 })
 export class MeusCarrosComponent implements OnInit {
 
-  constructor() { }
+    error = false;
+    veiculos: MeuVeiculo[] = [];
+    veiculo: MeuVeiculo;
+    state = 'ready';
+    titleModal = '';
 
-  ngOnInit(): void {
-  }
+    constructor(
+        private readonly meusCarrosService: MeusCarrosService,
+        private readonly toastr: ToastrService,
+
+    ) { }
+
+    ngOnInit(): void {
+        this.getMeusCarros();
+    }
+
+    getMeusCarros(): void {
+        this.meusCarrosService.getClienteVeiculos()
+            .subscribe(result => {
+                this.veiculos = result;
+            })
+    }
+
+    cadastrarVeiculo(veiculo): void {
+        this.meusCarrosService.cadastrarVeiculo(veiculo)
+            .subscribe(result => {
+                this.veiculos.push(result);
+            }, err => {
+                //
+            }, () => {
+                this.toastr.success('Veículo cadastrado com sucesso');
+            })
+    }
+
+    setVeiculo(veiculo: MeuVeiculo): void {
+        this.titleModal = `Remover ${veiculo.marca}`;
+        this.veiculo = veiculo;
+    }
+
+    removeVeiculo(): void {
+        this.meusCarrosService.removeVeiculo(this.veiculo.id)
+            .subscribe(result => {
+                this.veiculos.splice(this.veiculos.indexOf(this.veiculo), 1);
+            }, err => {
+                console.log(err)
+            }, () => {
+                this.toastr.success('Veículo removido com sucesso');
+            })
+    }
 
 }
--- src/app/pages/meus-carros/meus-carros.module.ts
+++ src/app/pages/meus-carros/meus-carros.module.ts
@@ -5,13 +5,21 @@ import { MeusCarrosRoutingModule } from './meus-carros-routing.module';
 import { MeusCarrosComponent } from './meus-carros.component';
 import { CoreModule } from 'src/app/core/core.module';
 import { MeuCarroFormComponent } from './meu-carro-form/meu-carro-form.component';
+import { MeusCarrosService } from './meus-carros.service';
+import { OsServicesNewService } from '../os-services/os-services-form/os-services-new.service';
+import { ModalModule } from 'src/app/shared/modal/modal.module';
 
 
 @NgModule({
-  declarations: [MeusCarrosComponent, MeuCarroFormComponent],
-  imports: [
-    CoreModule,
-    MeusCarrosRoutingModule
-  ]
+    declarations: [MeusCarrosComponent, MeuCarroFormComponent],
+    imports: [
+        ModalModule,
+        CoreModule,
+        MeusCarrosRoutingModule
+    ],
+    providers: [
+        OsServicesNewService,
+        MeusCarrosService,
+    ]
 })
 export class MeusCarrosModule { }
